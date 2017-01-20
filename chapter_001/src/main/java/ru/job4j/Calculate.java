9af829b6bd546db3011d9deba1f3f1decb7893bc
misterflud <yurolejniko@yandex.ru>
2017-01-20 13:49:01 +0300

    add comments and use code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+ *This is Class is created three string from one string
+ * @author Anton
+ * @version 3
+ */
 public class Calculate {
+    /**
+     *
+     * @param value just string
+     * @return concatenations three parameters and two spaces between their
+     */
     public String echo(String value) {
         return String.format("%s %s %s", value, value, value);
     }
