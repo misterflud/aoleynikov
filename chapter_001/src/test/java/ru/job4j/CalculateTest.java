fca8f27552841682b905d15f9c7170cd28889ffd
misterflud <yurolejniko@yandex.ru>
2017-01-21 15:24:36 +0300

    update, because now I have checkstyle

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -2,16 +2,25 @@ package ru.job4j;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
+/**This is ClassTests.
+ * @author Anton
+ * @version 2
+ */
 public class CalculateTest {
+    /**
+     * Test1 add.
+     */
     @Test
     public void whenSetStopThenReturnThreeStops() {
         Calculate cl = new Calculate();
         String result = cl.echo("stop");
         assertThat(result, is("stop stop stop"));
     }
-
+    /**
+     * Test2 add.
+     */
     @Test
     public void whenSetNullInEchoReturnTwoSpaces() {
         Calculate cl = new Calculate();
