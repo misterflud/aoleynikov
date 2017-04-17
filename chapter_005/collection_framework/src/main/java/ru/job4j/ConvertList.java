990845c533d5c6dd1acbc52ed4a6bf521328db09
misterflud <yurolejniko@yandex.ru>
2017-04-17 15:22:55 +0300

    add program

---
+++ chapter_005/collection_framework/src/main/java/ru/job4j/ConvertList.java
@@ -0,0 +1,53 @@
+package ru.job4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anton on 17.04.2017.
+ */
+public class ConvertList {
+    /**
+     * Get list.
+     * @param array array
+     * @return List
+     */
+    public ArrayList<Integer> toList (int[][] array) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int[] it1 : array) {
+            for (int it2 : it1) {
+                list.add(it2);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Get mas.
+     * @param list list
+     * @param rows rows
+     * @return mas.
+     */
+    public int[][] toArray (List<Integer> list, int rows) {
+        int count = list.size();
+        while (count % rows != 0) {
+            count++;
+        }
+        int[][] mass = new int[count / rows][rows];
+        for( int jj = 0; jj < mass[mass.length - 1].length; jj++) {
+            mass[mass.length - 1][jj] = 0;
+        }
+
+        int i = 0;
+        for (int j = 0; j < mass.length; j++) {
+            for (int jj = 0; jj < mass[j].length; jj++) {
+                if (i == list.size()) {
+                    break;
+                }
+                mass[j][jj] = list.get(i);
+                i++;
+            }
+        }
+        return mass;
+    }
+}
---
+++ chapter_005/collection_framework/src/main/test/ru/job4j/ConvertListTest.java
@@ -0,0 +1,43 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Anton on 17.04.2017.
+ */
+public class ConvertListTest {
+    /**
+     * Test.
+     */
+    @Test
+    public void whenThen() {
+        ConvertList convertList = new ConvertList();
+        final int[][] mass = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 0, 0}};
+        final int rows = 4;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int[] it1 : mass) {
+            for (int it2 : it1) {
+                list.add(it2);
+            }
+        }
+        ArrayList<Integer> result1 = convertList.toList(mass);
+        assertThat(result1, is(list));
+
+
+
+        list.remove(list.size() - 1); //удаляем нули
+        list.remove(list.size() - 1);
+        for (Integer it : list) {
+            System.out.print(it + " ");
+        }
+
+
+        final int[][] result2 = convertList.toArray(list, rows);
+        assertThat(result2, is(mass));
+    }
+}
