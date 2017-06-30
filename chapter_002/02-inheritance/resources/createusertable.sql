00b16cb1eeffb8df234e313a1bad14e8b25d5092
misterflud <yurolejniko@yandex.ru>
2017-06-30 17:40:04 +0300

    update

--- chapter_002/02-inheritance/resources/createusertable.sql
+++
@@ -1,3 +0,0 @@
-SELECT FROM pg_tables  WHERE tablename = 'items'
-create table items (id serial primary key, name character varying (100), description character varying (400));
-select * from pg_tables where tablename = items;
