b708a192be7fd7a14660229a2a648dd6bf311f35
Anton Oleynikov <yurolejniko@yandex.ru>
2017-08-16 15:10:02 +0300

    add program

---
+++ tasks/serejcaProject/src/main/java/ru/aoleynikov/PushingButton.java
@@ -0,0 +1,5 @@
+package ru.aoleynikov;
+
+public class PushingButton {
+
+}
---
+++ tasks/serejcaProject/src/main/java/ru/aoleynikov/StartApp.java
@@ -0,0 +1,9 @@
+package ru.aoleynikov;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
