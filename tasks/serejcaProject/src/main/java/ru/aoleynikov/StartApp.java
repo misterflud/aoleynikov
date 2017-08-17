eecb58c90c813cbdb7cd553fe0146ac480803019
Anton Oleynikov <yurolejniko@yandex.ru>
2017-08-17 09:17:54 +0300

    del eclipse files

--- tasks/serejcaProject/src/main/java/ru/aoleynikov/StartApp.java
+++ tasks/serejcaProject/src/main/java/ru/aoleynikov/StartApp.java
@@ -3,7 +3,10 @@ package ru.aoleynikov;
 public class StartApp {
 
 	public static void main(String[] args) {
-		
+		Manager manager = new Manager("https://www.google.ru");
+		manager.parsing();
 	}
+	
+	
 
 }
