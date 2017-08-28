5b7ae23c46fff670436e1a5f13b93c8a410f531e
Anton Oleynikov <yurolejniko@yandex.ru>
2017-08-28 09:37:45 +0300

    add description

--- chapter_007/threads/src/main/java/ru/job4j/AsynchronousOperation.java
+++ chapter_007/threads/src/main/java/ru/job4j/AsynchronousOperation.java
@@ -5,14 +5,28 @@ package ru.job4j;
  */
 public class AsynchronousOperation {
 
+	/**
+	 * Inner class.
+	 * @author Anton Oleynikov
+	 * created on 28.08.2017
+	 */
     public static final class Calculate implements Runnable {
-
+    	/**
+    	 * Name of thread.
+    	 */
         private String name;
 
+        /**
+         * Constructor.
+         * @param name
+         */
         public Calculate(String name) {
             this.name = name;
         }
-
+        
+        /**
+         * Run.
+         */
         @Override
         public void run() {
             AsynchronousOperation a = new AsynchronousOperation();
@@ -20,6 +34,11 @@ public class AsynchronousOperation {
 
         }
     }
+    
+    /**
+     * Start program.
+     * @param args
+     */
     public static void main(String[] args) {
         new Thread(new Calculate("Thread1 ")).start();
         new Thread(new Calculate("Thread2 ")).start();
@@ -34,6 +53,10 @@ public class AsynchronousOperation {
         */
     }
 
+    /**
+     * Printing name of thread and iteration.
+     * @param name
+     */
     public void print(String name) {
         for (int i = 0; i < 10; i++) {
             System.out.println(name + i);
