f83f042946fd54775af7d7d650e042c487309c9d
AntonNout <AntonNout@ANOUT>
2018-01-04 22:14:01 +0300

    adds mockito

--- chapter_009/crud/src/main/webapp/WEB-INF/views/UsersView.jsp
+++ chapter_009/crud/src/main/webapp/WEB-INF/views/UsersView.jsp
@@ -5,7 +5,7 @@
   Time: 21:19
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page import="aoleynikov.servlets.SingletonPrintOut" %>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
--- chapter_009/crud/src/test/java/aoleynikov/servlets/controller/UsersControllerTest.java
+++ chapter_009/crud/src/test/java/aoleynikov/servlets/controller/UsersControllerTest.java
@@ -1,23 +1,46 @@
 package aoleynikov.servlets.controller;
 
 import static org.junit.Assert.*;
+import javax.servlet.ServletException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 
 import aoleynikov.servlets.controller.UsersController;
+import aoleynikov.servlets.model.BaseUser;
+import aoleynikov.servlets.model.User;
 
 public class UsersControllerTest {
 
 	@Test
-	public void addUserTest() {
+	public void addUserTest() throws ServletException, IOException {
+		BaseUser baseUser = new User("admin");
+		HttpSession session = mock(HttpSession.class);
+		when(session.getAttribute("authUser")).thenReturn("admin");
+		
+		
 		UsersController usersController = new UsersController();
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		HttpServletResponse response = mock(HttpServletResponse.class);
-		usersController.d
+		
+		
+		when(request.getServletPath()).thenReturn("/getUser");
+		when(request.getSession()).thenReturn(session);
+		when(request.getParameter("login")).thenReturn("admin");
+		
+		
+		usersController.doGet(request, response);
+		
+		
+		System.out.println(((User) request.getAttribute("getEditUser")).getEmail());//вылетает ошибка, так как JNDI не находит context.xml
+		//assertThat(request.getAttribute("getEditUser"));
 	}
 
 }
