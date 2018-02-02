ca8bf6c3fdf7cf4265e6c510fa593bb711b56d0a
Anton Oleynikov <yurolejniko@yandex.ru>
2018-02-02 15:48:19 +0300

    updates

--- chapter_009/crud2/src/main/java/ru/job4j/controller/UserJsonController.java
+++ chapter_009/crud2/src/main/java/ru/job4j/controller/UserJsonController.java
@@ -1,19 +1,28 @@
 package ru.job4j.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 
+ * @author Anton Oleynikov
+ * created on 02.02.2018
+ */
 public class UserJsonController extends HttpServlet {
 	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		resp.setContentType("text/json");
+		PrintWriter writer = new PrintWriter(resp.getOutputStream());
+		writer.append("");
 	}
+	
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
