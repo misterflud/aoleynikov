28db1d0fc7f4600970798bfd716ad85c9fdc15b3
Anton_PC <Anton_PC@PC>
2018-02-25 22:07:30 +0300

    updates

--- chapter_011/crud3/src/main/java/aoleynikov/servlets/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Anton on 30.06.2017.
- */
-package aoleynikov.servlets;
--- chapter_011/crud3/src/main/java/aoleynikov/servlets/util/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Anton Oleynikov
- * created on 01.11.2017
- */
-package aoleynikov.servlets.util;
